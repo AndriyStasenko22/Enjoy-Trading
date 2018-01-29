06206d259760f0b726faa595013d22895264174e
Andriy Stasenko <andriystasenko@gmail.com>
2018-01-29 02:50:19 +0200

    Responsive

--- app/js/comon.js
+++ app/js/comon.js
@@ -1,16 +1,29 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
+    // кнопка "На верх"
     $('.up-button').on('click', function (e) {
         e.preventDefault();
-
-        $("html, body").animate({scrollTop:0}, '800');
+        $("html, body").animate({scrollTop: 0}, '800');
     })
 
+    // активний пункт головно меню
+    $(document).on('click', '.menu-list .menu-item', function (e) {
+        $('.menu-list li.active').removeClass('active');
+        $(this).parent('li').addClass('active');
+        if ($(this).attr('href') == '#contact') {
+            e.preventDefault();
+            var link_href = $(this).attr('href');
+            var contact_block = $(link_href);
+            var contact_block_position = contact_block.offset().top;
+            $("html, body").animate({scrollTop: contact_block_position}, 500);
+        }
+    });
 
     var menu_selector = ".navigation-list";
-    function onScroll(){
+
+    function onScroll() {
         var scroll_top = $(document).scrollTop();
-        $(menu_selector + " a").each(function(){
+        $(menu_selector + " a").each(function () {
             var hash = $(this).attr("href");
             var target = $(hash);
             if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top) {
@@ -21,9 +34,10 @@ $(document).ready(function() {
             }
         });
     }
+
     $(document).ready(function () {
         $(document).on("scroll", onScroll);
-        $(menu_selector +" a").click(function(e){
+        $(menu_selector + " a").click(function (e) {
             e.preventDefault();
             $(document).off("scroll");
             $(menu_selector + " li.active").removeClass("active");
@@ -32,10 +46,20 @@ $(document).ready(function() {
             var target = $(hash);
             $("html, body").animate({
                 scrollTop: target.offset().top
-            }, 500, function(){
+            }, 500, function () {
                 window.location.hash = hash;
                 $(document).on("scroll", onScroll);
             });
         });
+    });
+
+    $(document).on('click', '.menu-mobile-button', function (e) {
+        e.preventDefault();
+        $('.mobile-menu').addClass('open');
+    });
+
+    $(document).on('click', '.mobile-menu.open .menu-close-button', function (e) {
+        e.preventDefault();
+        $('.mobile-menu.open').removeClass('open');
     });
 });
